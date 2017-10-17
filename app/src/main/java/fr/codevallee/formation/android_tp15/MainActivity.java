5a388951436da632351de7ca29dfc2ab0b3026cc
Thibault Goudouneix <nilmanduil@gmail.com>
2017-10-17 14:07:28 +0200

    Externalised progressbar actualization with Handler pattern

--- app/src/main/java/fr/codevallee/formation/android_tp15/MainActivity.java
+++ app/src/main/java/fr/codevallee/formation/android_tp15/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.codevallee.formation.android_tp15;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -23,15 +26,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpProgress() {
-        for (int i = 0; i <= 100; i++) {
-            ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-            progressBar.setProgress(i);
-            try {
-                Thread.sleep(300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        final Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                int progress = msg.getData().getInt("PROGRESS");
+                progressBar.setProgress(progress);
             }
-        }
+        };
+
+        Thread progressThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 100; i++) {
+                    Message message = handler.obtainMessage();
+                    Bundle messageBundle = new Bundle();
+                    messageBundle.putInt("PROGRESS", i);
+                    message.setData(messageBundle);
+                    handler.sendMessage(message);
+                    Log.d("Info", "Progress : " + i + "%");
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        progressThread.start();
     }
 }
